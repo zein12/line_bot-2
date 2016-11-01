d3375942bc1e0d6cd4e41bb6bed16de1a67bf633
石井翼 <teardrop@SGGT.local>
2016-11-01 15:31:06 +0900

    db13

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-    $url = parse_url(getenv("mysql://<b217bd08555b23>:<4a2fa5be>@<us-cdbr-iron-east-04.cleardb.net>/<heroku_bacf636d511d4fe>?reconnect=true"));
+    $url = parse_url(getenv("mysql2://<b217bd08555b23>:<4a2fa5be>@<us-cdbr-iron-east-04.cleardb.net>/<heroku_bacf636d511d4fe>?reconnect=true"));
 
     $server = $url["host"];
     $username = $url["user"];
