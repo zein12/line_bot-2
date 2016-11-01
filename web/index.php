51c3f947ffb43f64e34a262a52ebb068d7c2a968
石井翼 <teardrop@SGGT.local>
2016-11-01 12:17:13 +0900

    db4

--- web/index.php
+++ web/index.php
@@ -1,15 +1,33 @@
 <?php
-    $url = parse_url(getenv("mysql://b217bd08555b23:4a2fa5be@us-cdbr-iron-east-04.cleardb.net/heroku_bacf636d511d4fe?reconnect=true"));
 
-    $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-    $username = $url["b217bd08555b23"];
-    $password = $url["697b02c3e63d6b5"];
-    $db = substr($url["heroku_bacf636d511d4fe"], 1);
+$db = parse_url($_SERVER['mysql://b217bd08555b23:4a2fa5be@us-cdbr-iron-east-04.cleardb.net/heroku_bacf636d511d4fe?reconnect=true']);
+$db['dbname'] = ltrim($db['heroku_bacf636d511d4fe'], '/');
+$dsn = "mysql:host={$db['us-cdbr-iron-east-04.cleardb.net']};dbname={$db['dbname']};charset=utf8";
 
-    $link = mysqli_connect($server, $username, $password, $db);
-    $result = mysqli_query($link, "select * from user");
+try {
+    $db = new PDO($dsn, $db['b217bd08555b23'], $db['97b02c3e63d6b5']);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    while($user = mysqli_fetch_array($result)) {
-      echo $user['id'], " : ", $user['name'], "<br>";
+    $sql = 'SELECT * FROM user';
+    $prepare = $db->prepare($sql);
+    $prepare->execute();
+
+    echo '<pre>';
+    $prepare->execute();
+    $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    print_r(h($result));
+    echo "\n";
+    echo '</pre>';
+} catch (PDOException $e) {
+    echo 'Error: ' . h($e->getMessage());
+}
+
+function h($var)
+{
+    if (is_array($var)) {
+        return array_map('h', $var);
+    } else {
+        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
     }
-?>
+}
