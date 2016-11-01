bb300a80860ae7a55634acf4fb213cf7b911b666
石井翼 <teardrop@SGGT.local>
2016-11-01 12:04:27 +0900

    db2

--- web/index.php
+++ web/index.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>こちらはLINE Messaging APIのデモサイトです。</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-こちらはLINE Messaging APIのデモサイトです。
-</body>
-</html>
+<?php
+
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+
+try {
+    $db = new PDO($dsn, $db['user'], $db['pass']);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT * FROM user';
+    $prepare = $db->prepare($sql);
+    $prepare->execute();
+
+    echo '<pre>';
+    $prepare->execute();
+    $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    print_r(h($result));
+    echo "\n";
+    echo '</pre>';
+} catch (PDOException $e) {
+    echo 'Error: ' . h($e->getMessage());
+}
+
+function h($var)
+{
+    if (is_array($var)) {
+        return array_map('h', $var);
+    } else {
+        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+    }
+}
