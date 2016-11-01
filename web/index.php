315fca0d18672e1c5b51bb9984b3e7bb55c3aa2c
石井翼 <teardrop@SGGT.local>
2016-11-01 15:24:31 +0900

    db11

--- web/index.php
+++ web/index.php
@@ -1,15 +1,14 @@
 <?php
     $url = parse_url(getenv("mysql://b217bd08555b23:4a2fa5be@us-cdbr-iron-east-04.cleardb.net/heroku_bacf636d511d4fe"));
 
-    $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
+    $server = $url["host"];
     $db = substr($url["path"], 1);
 
     $link = mysqli_connect($server, $username, $password, $db);
     $result = mysqli_query($link, "select * from user");
 
-echo "test";
 
     while($user = mysqli_fetch_array($result)) {
       echo $user['id'], " : ", $user['name'], "<br>";
