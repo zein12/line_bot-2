2d66341aa9e0bc08066443bdbc1a3d704af2109a
石井翼 <teardrop@SGGT.local>
2016-11-01 15:16:51 +0900

    db9

--- web/index.php
+++ web/index.php
@@ -1,16 +1,13 @@
 <?php
     $url = parse_url(getenv("mysql://b217bd08555b23:4a2fa5be@us-cdbr-iron-east-04.cleardb.net/heroku_bacf636d511d4fe?reconnect=true"));
 
-    $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-    $username = $url["b217bd08555b23"];
-    $password = $url["697b02c3e63d6b5"];
-    $db = substr($url["heroku_bacf636d511d4fe"], 1);
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
 
     $link = mysqli_connect($server, $username, $password, $db);
     $result = mysqli_query($link, "select * from user");
-
-echo "test";
-echo $username;
 
     while($user = mysqli_fetch_array($result)) {
       echo $user['id'], " : ", $user['name'], "<br>";
