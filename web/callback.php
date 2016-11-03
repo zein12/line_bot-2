9025714a5ff697c40286b81ecf53fdbab66aaddd
石井翼 <teardrop@SGGT.local>
2016-11-03 20:29:04 +0900

    db6

--- web/callback.php
+++ web/callback.php
@@ -84,6 +84,9 @@ function DoActionAll($message_text){
   } else if ("@rule" == $message_text) {
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ルール説明だよ");
     $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+  } else if ("@db" == $message_text) {
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$user[id]");
+    $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
   }
 }
 //BeforeのDoAction,メッセージを見てアクションする
