559318b6ba6b2c10d1dada7bf6b84becad0441f8
石井翼 <teardrop@SGGT.local>
2016-11-03 21:06:49 +0900

    db10

--- web/callback.php
+++ web/callback.php
@@ -83,10 +83,10 @@ function DoActionAll($message_text){
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ルール説明だよ");
     $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
   } else if ("@db" == $message_text) {
-    $result = mysqli_query($link, "select * from user");
+    $result = mysqli_query($link, "select id from user where id = 3;");
     $row = mysqli_fetch_array($result, MYSQLI_NUM);
     //printf ("%s (%s)\n", $row[0], $row[1]);
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("%s (%s)\n", $row[0], $row[1], $row[2]);
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("%s\n", $row[0]);
     $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
   }
 }
