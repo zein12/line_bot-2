82ee903c90bbfa72a4ee5a563dc3b95e33fdf449
石井翼 <teardrop@SGGT.local>
2016-11-04 18:20:07 +0900

    no message

--- web/callback.php
+++ web/callback.php
@@ -171,7 +171,7 @@ function DoActionWaiting($message_text){
             $profile = $response->getJSONDecodedBody();
             $user_name = mysqli_real_escape_string($link, $profile['displayName']);
             $user_id = mysqli_real_escape_string($link, $event->source->userId);
-            $room_num = mysqli_real_escape_string($link, $row[0]);
+            //$room_num = mysqli_real_escape_string($link, $row[0]);
             //$result = mysqli_query($link, "insert into user (user_id, user_name, game_room_num, role, voted_num, is_roling, is_voting) values ('$user_id', '$user_name', '$room_num', '無し', 0, 'false', 'false');");
         }
       }
