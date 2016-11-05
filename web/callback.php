797e20d141a449acb00c70cc7c9e34c22234a8d0
ashi-psn <psn.arcz.vtzf@gmail.com>
2016-11-06 05:22:35 +0900

    add ロギング追加

--- web/callback.php
+++ web/callback.php
@@ -5,6 +5,9 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
 use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 // データベース
 // ・game_room
 // game_room_num(Int) game_room_id(String) game_mode(String) num_of_people(Int) num_of_roles(Int) num_of_votes(Int)
@@ -151,6 +154,10 @@ function DoActionAll($message_text){
       */
       error_log("hello, this is a test!");
       $userlist = CarouselModel::sendCarousel($gameRoomId,$link);
+      $log = new Logger('name');
+      $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+      $log->addWarning('Foo');
+      $log->addWarning(print_r($userlist,true));
       error_log(print_r($userlist,true));
       file_put_contents("php://stderr", print_r($userlist,true));
       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($userlist["user_id"]);
