156f68e2981dae00c857fc4f85370bbd869d8b80
石井翼 <teardrop@SGGT.local>
2016-11-06 03:06:38 +0900

    no message

--- web/callback.php
+++ web/callback.php
@@ -200,16 +200,16 @@ function DoActionAll($message_text){
 
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('vYskIIIna79UwhpXsYtI3Xd8LsBWIrYwurJ6bWajgIKK9o7hXJuYAAl16uw8E1+9RuwuNHMPU/JEv2bL9FSu6hglkLY+fTZsSCtiEqsObUsZtUf1Hp7mmPZmttk8REBs4635vsMjsrd21TXyEN8iTQdB04t89/1O/w1cDnyilFU=');
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e051f306f6d42b66e715790b82e0544d']);
-    $response = $bot->leaveGroup($gameRoomId);
-    error_log(var_dump($response));
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("leaveだよ");
-    $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-    if($response -> getHTTPStatus() == 200){
-
-      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("leaveだよ");
-      $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-
-    }
+    // $response = $bot->leaveGroup($gameRoomId);
+    // error_log(var_dump($response));
+    // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("leaveだよ");
+    // $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    // if($response -> getHTTPStatus() == 200){
+    //
+    //   $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("leaveだよ");
+    //   $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    //
+    // }
     //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
